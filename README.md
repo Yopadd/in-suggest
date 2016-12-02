0a69ac19b51fc2aecd85741f0657383ca5ff5a07
Yoann <yoann.bourdex@gmail.com>
2016-12-02 16:47:01 +0100

    first

---
+++ README.md
@@ -0,0 +1,64 @@
+in-suggest
+=================
+
+in-suggest is simple input autocomplete, typeahead module.
+
+Install
+-------
+Get file in `./dist`
+```html
+<link rel="stylesheet" href="in-suggest.min.css">
+<script src="in-suggest.js"></script>
+```
+
+Using module
+```
+npm install in-suggest
+```
+```js
+import InSuggest from 'in-suggest'
+```
+
+Usage
+------
+```html
+<input id="my-input" type="text" placeholder="Enter a value">
+```
+```js
+const myInput = new InSuggest('my-input', options)
+```
+
+### options
+
+- __action(query, next)__ is fired when a key is pressed - _required_
+    - __query__ - the current input value
+    - __next__ - a callback function. you must give him an array
+
+- __selected(data, next)__ is fired when an item is selected - _required_
+    - __data__ - the object selected
+    - __next__ - a callback function that takes a string in paramter. This string parameter set the input value
+
+- __createItem(data)__ is called for each data passed by `action()` - _required_
+
+#### Example
+```js
+const myData = ['foo', 'bar']
+const simpleAutocomplete = new InSuggest('id', {
+    action(query, next) {
+        // return myData filtered by the current input value
+        next(myData.filter(
+            value => value.toLowerCase().startsWith(query.toLowerCase()))
+        )
+    },
+
+    selected(d, next) {
+        next(d) // d is 'foo' or 'bar' and the input value take d
+    },
+
+    createItem(d) {
+        const item = document.createElement('div')
+        item.innerHTML = d
+        return item
+    }
+})
+```
---
+++ index.js
@@ -0,0 +1,174 @@
+'use strict';
+
+class Menu {
+    constructor(el, parent) {
+        this.parent = parent;
+        this.el = el;
+        this.el.className = 'in-suggest__menu is-hidden';
+        this.el.addEventListener('mouseenter', () => {
+            this.el.classList.add('is-hover');
+        });
+        this.el.addEventListener('mouseleave', () => {
+            this.el.classList.remove('is-hover');
+        });
+    }
+
+    hidden(bool = true) {
+        if (bool) this.el.classList.add('is-hidden');
+        else this.el.classList.remove('is-hidden');
+        this.clear();
+    }
+
+    clear() {
+        this.parent.index = -1;
+        this.el.innerHTML = '';
+    }
+}
+
+class Item {
+    constructor(item, data, index, parent) {
+        if (!(item instanceof HTMLElement)) return this.logError('"createItem" should be return a HTMLElement')
+
+        this.parent = parent;
+        this.data = data;
+        this.id = index;
+        this.el = document.createElement('div');
+        this.el.className = 'in-suggest__menu__item';
+        this.el.appendChild(item);
+
+        this.el.addEventListener('click', this.parent.click.bind(this.parent, this.data));
+        this.el.addEventListener('mouseenter', () => {
+            this.parent.index = index;
+            this.el.classList.add('is-active');
+        });
+        this.el.addEventListener('mouseleave', () => {
+            this.parent.index = -1;
+            this.el.classList.remove('is-active');
+        });
+
+        this.parent.menu.el.appendChild(this.el);
+    }
+
+    active(bool = true) {
+        if (!bool) {
+            this.el.classList.remove('is-active');
+            return
+        }
+        this.parent.items.forEach(item => item.active(false));
+        this.el.classList.add('is-active');
+    }
+}
+
+class InSuggest {
+    constructor(id, options = {}) {
+        this.input = document.getElementById(id);
+
+        if (!this.input)
+            return this.logError(`element with "${id}" is not found`)
+        if (this.input.getAttribute('type') !== 'text')
+            return this.logError('element input should be have type text')
+        if (!options.action || !options.createItem || !options.selected)
+            return this.logError('options "action", "createItem" and "click" is required')
+
+        this.input.className = 'in-suggest__input';
+
+        this.wrapper = document.createElement('div');
+        this.wrapper.className = 'in-suggest';
+        this.wrapper.id = this.input.id;
+        this.input.removeAttribute('id');
+
+        this.menu = new Menu(document.createElement('div'), this);
+
+        this.input.parentNode.insertBefore(this.wrapper, this.input);
+        this.wrapper.appendChild(this.input);
+        this.wrapper.appendChild(this.menu.el);
+
+        this.items = [];
+        this.index = -1;
+        this.timer = null;
+        this.loadState = false;
+
+        this.options = options;
+
+        this.input.addEventListener('input', this.action.bind(this, options.action));
+        this.input.addEventListener('keydown', this.down.bind(this));
+        this.input.addEventListener('keydown', this.up.bind(this));
+        this.input.addEventListener('keyup', this.enter.bind(this));
+        this.input.addEventListener('keyup', this.escape.bind(this));
+        this.input.addEventListener('focus', this.action.bind(this, options.action));
+        this.input.addEventListener('blur', this.blur.bind(this));
+    }
+
+    blur() {
+        if (!this.menu.el.classList.contains('is-hover')) this.menu.hidden();
+    }
+
+    down(e) {
+        if (e.key !== 'ArrowDown') return
+        if (this.index < this.items.length - 1) this.index++;
+        else this.index = 0;
+        this.items.find(item => item.id === this.index).active();
+        e.preventDefault();
+    }
+
+    up(e) {
+        if (e.key !== 'ArrowUp') return
+        if (this.index > 0) this.index--;
+        else this.index = this.items.length - 1;
+        this.items.find(item => item.id === this.index).active();
+        e.preventDefault();
+    }
+
+    enter(e) {
+        if (e.key !== 'Enter' || this.index === -1 || this.loadState) return
+        this.click(this.items[this.index].data);
+    }
+
+    escape(e) {
+        if (e.key !== 'Escape') return
+        this.input.value = '';
+        this.menu.hidden();
+    }
+
+    click(item) {
+        this.options.selected(item, str => {
+            if (typeof str !== 'string')
+                return this.logError('"click" should be return a callback with string parameter')
+            this.input.value = str;
+            this.menu.hidden();
+        });
+    }
+
+    action(callback) {
+        clearTimeout(this.timer);
+        this.loading(false);
+
+        this.menu.hidden(false);
+
+        this.loading(true);
+        this.timer = setTimeout(() => {
+            callback.call(this, this.input.value, results => {
+                if (!Array.isArray(results)) this.items = [results];
+                else this.items = results;
+
+                this.loading(false);
+                this.menu.clear();
+                this.items = this.items.map((item, index) =>
+                    new Item(this.options.createItem.call(this, item), item, index, this)
+                );
+            });
+        }, 200);
+    }
+
+    loading(bool) {
+        this.loadState = bool;
+        if (bool) this.wrapper.classList.add('is-loading');
+        else this.wrapper.classList.remove('is-loading');
+    }
+
+    logError(message) {
+        throw new Error(`[in-suggest] ${message}`)
+    }
+}
+
+module.exports = InSuggest;
